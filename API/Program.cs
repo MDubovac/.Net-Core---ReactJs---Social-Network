f47ba3842d0cdf61d28b4d07d2c398823d6dfc43
MDubovac <mladendubovac98@gmail.com>
2023-01-06 18:45:29 +0100

    Added Activities CRUD API

--- API/Program.cs
+++ API/Program.cs
@@ -1,27 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// DbContext
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-// Cors policy
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddAplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -33,6 +18,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors("CorsPolicy");
+
 
 //app.UseHttpsRedirection();
 
